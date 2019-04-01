e6e1ecfc195329f9ea878259eae10db0031b0751
Waffas1138 <49158487+Waffas1138@users.noreply.github.com>
2019-04-01 10:03:34 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_Travis Bivins.py
@@ -0,0 +1,16 @@
+# 04/01/19
+# CTI-110 P2T1 - Sales Prediction
+# Travis Bivins
+# get the projected total sales.
+total_sales = float( input(' Enter the Project sales: ') )
+
+# Calculate the profts as 23 percent of total sales.
+profit = total_sales * 0.23
+
+
+# Display the proft.
+print('The profit is $', format(profit, ',.2f'))
+
+
+
+
